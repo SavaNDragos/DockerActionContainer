4253c1d2b2b29e3aa2f5bb794e046f72ed4b2fc7
Sava N Dragos <sava.n.dragos@hotmail.com>
2026-09-25 21:45:53 +0300

    testing outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/sh -l
 echo "Hello $@"
+echo "answer=42" >> $GITHUB_OUTPUT
+echo "### hello $@! :rocket:" >> $GITHUB_STEP_SUMMARY
+echo "<h3> The answer from the Deep Thought is 42 :robot:</h3>" >> $GITHUB_STEP_SUMMARY
